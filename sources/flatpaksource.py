9c3b245c916eed4b58255ed21c70d62e673e4370
Jordan Leppert <jordanleppert@gmail.com>
2019-01-21 16:12:46 +0000

    Many fixes for flatpak, now handles branches, properly uses checksum to get status

--- sources/flatpaksource.py
+++ sources/flatpaksource.py
@@ -8,7 +8,7 @@ import re
 
 class FlatpakSource(AbstractSource):
 
-    ids_regex = re.compile(r'^(\S+)\s+(\S+)\s+([^\-]+?)\s+\-\s+(.+)$')
+    ids_regex = re.compile(r'^(\S+)\s+(\S+)\s+(\S+)\s+([^\-]+?)\s+\-\s+(.+)$')
     description_regex = re.compile(r'^\s*([^\:]+?)\:\s+(.+)$')
     name_description_regex = re.compile(r'^([^\-]+)\s+\-\s+(.+)$')
 
@@ -23,10 +23,10 @@ class FlatpakSource(AbstractSource):
         pass
 
     def search(self, name):
-        table = self.executor.call("flatpak search \"{0}\" --columns=application,remotes,description".format(name), self.ids_regex, None, True)
+        table = self.executor.call("flatpak search \"{0}\" --columns=remotes,application,branch,description".format(name), self.ids_regex, None, True)
         results = []
         for row in table:
-            appid = row[1] + ':' + row[0]
+            appid = row[0] + ":" + row[1] + ":" + row[2]
             app = self.getapp(appid)
             if app is None:
                 raise Exception("Could not find app {0} in results".format(appid))
@@ -36,18 +36,18 @@ class FlatpakSource(AbstractSource):
     def local(self, name):
         installedids = self._get_installed_ids()
         results = []
-        for id in installedids:
-            localapp = installedids[id]
+        for appid in installedids:
+            localapp = installedids[appid]
             if name is None or localapp.match(name):
-                app = self.getapp(id)
+                app = self.getapp(appid)
                 if app is None:
                     raise Exception("Could not find app {0} in results".format(appid))
                 results.append(app)
         return results
 
     def getapp(self, appid):
-        remote, id = self._split_id(appid)
-        app = FlatpakApp(self, appid, '', '', None, None, None, None, None)
+        remote, id, branch = self._split_id(appid)
+        app = FlatpakApp(self, appid, '', '', None, None, None, None)
 
         output = self.executor.call("flatpak info {0}".format(id), None, None, True, [0, 1])
         for line in output.splitlines():
@@ -65,10 +65,8 @@ class FlatpakSource(AbstractSource):
                     app.installed = row[1]
                 if row[0] == 'Commit':
                     app.local_checksum = row[1]
-                if row[0] == 'Branch':
-                    app.branch = row[1]
 
-        output = self.executor.call("flatpak remote-info {0} {1}".format(remote, id), None, None, True, [0, 1])
+        output = self.executor.call("flatpak remote-info {0} {1}//{2}".format(remote, id, branch), None, None, True, [0, 1])
         for line in output.splitlines():
             match = self.name_description_regex.match(line)
             if match:
@@ -84,24 +82,22 @@ class FlatpakSource(AbstractSource):
                     app.version = row[1]
                 if row[0] == 'Commit':
                     app.remote_checksum = row[1]
-                if row[0] == 'Branch' and app.branch is None:
-                    app.branch = row[1]
         
-        if app.branch is None:
+        if app.local_checksum is None and app.remote_checksum is None:
             return None
         return app
 
     def install(self, app):
-        remote, id = self._split_id(app.id)
+        remote, id, branch = self._split_id(app.id)
         self.executor.call("flatpak install -y {0} {1}".format(remote, id))
 
     def remove(self, app):
-        remote, id = self._split_id(app.id)
+        remote, id, branch = self._split_id(app.id)
         self.executor.call("flatpak remove -y {0}".format(id))
 
     def update(self, apps, autoconfirm):
         for app in apps:
-            remote, id = self._split_id(app.id)
+            remote, id, branch = self._split_id(app.id)
             self.executor.call("flatpak update --assumeyes {0}".format(id))
         return None
 
@@ -109,29 +105,31 @@ class FlatpakSource(AbstractSource):
         if ':' not in appid:
             raise Exception("{0} is not a valid Flatpak app ID".format(appid))
         i = appid.index(':')
-        return (appid[0:i], appid[i + 1:])
+        if ':' not in appid[i + 1:]:
+            raise Exception("{0} is not a valid Flatpak app ID".format(appid))
+        ii = appid.index(':', i + 1)
+        return (appid[0:i], appid[i + 1:ii], appid[ii + 1:])
 
     def _get_installed_ids(self):
-        table = self.executor.call("flatpak list --columns=application,origin,description", self.ids_regex, None, True)
+        table = self.executor.call("flatpak list --columns=origin,application,branch,description", self.ids_regex, None, True)
         results = {}
         for row in table:
-            id = row[1] + ":" + row[0]
-            results[id] = FlatpakApp(self, id, row[2], row[3], None, None, None, None, None)
+            appid = row[0] + ":" + row[1] + ":" + row[2]
+            results[appid] = FlatpakApp(self, appid, row[3], row[4], None, None, None, None)
         return results
 
 
 class FlatpakApp(App):
 
-    def __init__(self, source, id, name, desc, version, installed, remote_checksum, local_checksum, branch):
+    def __init__(self, source, id, name, desc, version, installed, remote_checksum, local_checksum):
         super().__init__(source, id, name, desc, version, installed)
         self.remote_checksum = remote_checksum
         self.local_checksum = local_checksum
-        self.branch = branch
 
     def status(self):
         indicator = 'N'
-        if self.installed is not None:
+        if self.local_checksum is not None:
             indicator = 'I'
-            if self.remote_checksum != self.local_checksum:
+            if self.remote_checksum != self.local_checksum and self.remote_checksum is not None:
                 indicator = 'U'
         return indicator
