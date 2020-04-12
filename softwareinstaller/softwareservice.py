acf8ab99af85653dce81a330b559b6245f263e25
Jordan Leppert <jordanleppert@gmail.com>
2020-04-12 11:36:04 +0100

    Uncommenting update

--- softwareinstaller/softwareservice.py
+++ softwareinstaller/softwareservice.py
@@ -193,25 +193,25 @@ class SoftwareService:
         for task in self.config['update.tasks.pre']:
             self.executor.call(task, stdout=self.output_std, stderr=self.output_err)
 
-        # for sourceid in apps.copy().keys():
-        #     source = self.getsource(sourceid)
-        #     updatedlist = source.update(apps[sourceid], autoconfirm)
-        #     if updatedlist is not None:
-        #         apps[sourceid] = updatedlist
-        #         return apps
+        for sourceid in apps.copy().keys():
+            source = self.getsource(sourceid)
+            updatedlist = source.update(apps[sourceid], autoconfirm)
+            if updatedlist is not None:
+                apps[sourceid] = updatedlist
+                return apps
             
-        #     if source.check_updated:
-        #         apps_still_to_be_updated = self.local(None, ['U'], [source])
-        #         if sourceid in apps_still_to_be_updated:
-        #             foundids = dict([(a.id, a.version) for a in apps_still_to_be_updated[sourceid]])
-        #             for app in apps[sourceid]:
-        #                 if app.id in foundids and app.version == foundids[app.id]:
-        #                     raise Exception("App {0} wasn't successfully updated".format(app.id))
+            if source.check_updated:
+                apps_still_to_be_updated = self.local(None, ['U'], [source])
+                if sourceid in apps_still_to_be_updated:
+                    foundids = dict([(a.id, a.version) for a in apps_still_to_be_updated[sourceid]])
+                    for app in apps[sourceid]:
+                        if app.id in foundids and app.version == foundids[app.id]:
+                            raise Exception("App {0} wasn't successfully updated".format(app.id))
 
-        #     del apps[sourceid]
+            del apps[sourceid]
 
-        # for notifier in self.notifiers:
-        #     notifier.notify()
+        for notifier in self.notifiers:
+            notifier.notify()
 
         for task in self.config['update.tasks.post']:
             self.executor.call(task, stdout=self.output_std, stderr=self.output_err)
