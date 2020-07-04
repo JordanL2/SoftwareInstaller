ec392fcc2e00122a86a84d6333c3917bbc49c61b
Jordan Leppert <jordanleppert@gmail.com>
2020-07-04 18:42:35 +0100

    Fixed weird bug reading an output stream from a proc

--- softwareinstaller/commandexecutor.py
+++ softwareinstaller/commandexecutor.py
@@ -4,11 +4,15 @@ import subprocess
 import os
 import fcntl
 
+import time
 
-def non_blocking_read(output):
+
+def set_non_blocking(output):
     fd = output.fileno()
     fl = fcntl.fcntl(fd, fcntl.F_GETFL)
     fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+
+def non_blocking_read(output):
     try:
         return output.readlines()
     except:
@@ -29,6 +33,9 @@ class CommandExecutor:
         stderr_result = ''
         result_code = None
 
+        set_non_blocking(proc.stdout)
+        set_non_blocking(proc.stderr)
+
         while True:
             lines = non_blocking_read(proc.stdout)
             for line in lines:
@@ -45,6 +52,9 @@ class CommandExecutor:
             if result_code is not None:
                 break
             result_code = proc.poll()
+
+            # This fixes a weird bug causing output not to be read correctly
+            time.sleep(0.000001)
 
         stdout_result = stdout_result.rstrip("\n")
         stderr_result = stderr_result.rstrip("\n")
@@ -72,7 +82,7 @@ class CommandExecutor:
                     result_line = [converters[i](r) for i, r in enumerate(result_line)]
                 response.append(result_line)
             elif not ignorenomatch:
-                raise Exception("Line '{0}' did not match regex".format(line))
+                raise Exception("Line '{0}' did not match regex (command: {1})".format(line, command))
         return response
 
     def getuser(self):
