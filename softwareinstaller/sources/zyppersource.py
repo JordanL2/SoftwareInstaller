8990a99688e13055b314c64c7cc80915dc4239a9
Jordan Leppert <jordanleppert@gmail.com>
2021-10-15 17:59:57 +0100

    Zypper - local is working

--- softwareinstaller/sources/zyppersource.py
+++ softwareinstaller/sources/zyppersource.py
@@ -8,7 +8,8 @@ import re
 
 class ZypperSource(AbstractSource):
     
-    installed_regex = re.compile(r'^(\S+)\s*\|\s*(\S+)\s*\|\s*(\S+)\s*\|\s*(\S+)\s*\|\s*(\S+)\s*\|\s*(\S+)\s*$')
+    installed_regex = re.compile(r'^(\S+)\s*\|\s*(\S+)\s*\|\s*(\S+)\s*\|\s*(\S+)\s*\|\s*(\S+)\s*\|\s*(.+)$')
+    info_name = re.compile(r'Information for package (\S+):')
     info_version = re.compile(r'Version\s+:\s+(\S+)')
     info_installed_uptodate = re.compile(r'Status\s*:\s*up\-to\-date\s*')
     info_installed_outofdate = re.compile(r'Status\s*:\s*out\-of\-date\s+\(version (\S+) installed\)\s*')
@@ -36,43 +37,75 @@ class ZypperSource(AbstractSource):
     def local(self, terms):
         start_time = time.perf_counter()
 
-       
+        results = []
+        installedids = self._get_installed_ids()
+        apps = self.getapps(installedids.keys())
+        for app in apps:
+            if app is not None and (terms is None or app.match(terms)):
+                results.append(app)
 
         self.log('performance', "zypper local {}".format(time.perf_counter() - start_time))
         return results
 
-    def getapp(self, appid, installedids=None, skipremote=False):
+    def getapp(self, appid):
         start_time = time.perf_counter()
+        
+        self.log('performance', "zypper getapp {}".format(time.perf_counter() - start_time))
+        return self.getapps([appid])[0]
 
+    def getapps(self, appids):
+        start_time = time.perf_counter()
+        
+        apps = []
+
+        appid = None
         desc = None
         available_version = None
         installed_version = None
         
         found_desc = False
-        out = self.executor.call("zypper info {0}".format(appid))
+        out = self.executor.call("zypper info {0}".format(' '.join(appids)))
         for line in out.split("\n"):
+            name_match = self.info_name.match(line)
+            if name_match:
+                if appid in appids:
+                    apps.append(App(self, appid, appid, desc, available_version, installed_version))
+                    
+                appid = name_match.group(1)
+                desc = None
+                available_version = None
+                installed_version = None
+                found_desc = False
+                continue
+
             if found_desc:
                 desc += line.strip()
                 continue
+
             version_match = self.info_version.match(line)
             if version_match:
                 available_version = version_match.group(1)
                 continue
+
             installed_uptodate_match = self.info_installed_uptodate.match(line)
             if installed_uptodate_match:
                 installed_version = available_version
                 continue
+
             installed_outofdate_match = self.info_installed_outofdate.match(line)
             if installed_outofdate_match:
                 installed_version = installed_outofdate_match.group(1)
                 continue
+
             desc_match = self.info_desc.match(line)
             if desc_match:
                 found_desc = True
                 desc = ''
         
-        self.log('performance', "zypper getapp {}".format(time.perf_counter() - start_time))
-        return App(self, appid, appid, desc, available_version, installed_version)
+        apps.append(App(self, appid, appid, desc, available_version, installed_version))
+        
+        self.log('performance', "zypper getapps {}".format(time.perf_counter() - start_time))
+        return apps
 
     def install(self, app):
         self.executor.call("zypper install -y {0}".format(app.id), stdout=self.service.output_std, stderr=self.service.output_err)
@@ -87,5 +120,5 @@ class ZypperSource(AbstractSource):
 
     def _get_installed_ids(self):
         cmd = "zypper search --installed-only --details"
-        table = self.executor.call(cmd, self.installed_regex)
+        table = self.executor.call(cmd, self.installed_regex, None, True)
         return dict([(row[1], row[3]) for row in table])
