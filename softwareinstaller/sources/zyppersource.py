d63fe2040cb115e7c7741ebfc4458e4da6e15452
Jordan Leppert <jordanleppert@gmail.com>
2021-10-15 19:02:14 +0100

    Rewrote local to be a lot faster

--- softwareinstaller/sources/zyppersource.py
+++ softwareinstaller/sources/zyppersource.py
@@ -9,11 +9,15 @@ import re
 class ZypperSource(AbstractSource):
     
     installed_regex = re.compile(r'^(\S+)\s*\|\s*(\S+)\s*\|\s*(\S+)\s*\|\s*(\S+)\s*\|\s*(\S+)\s*\|\s*(.+)$')
-    info_name = re.compile(r'Information for package (\S+):')
     info_version = re.compile(r'Version\s+:\s+(\S+)')
     info_installed_uptodate = re.compile(r'Status\s*:\s*up\-to\-date\s*')
     info_installed_outofdate = re.compile(r'Status\s*:\s*out\-of\-date\s+\(version (\S+) installed\)\s*')
     info_desc = re.compile(r'Description\s*:\s*')
+    local_name = re.compile(r'\s*name:\s*(\S+)\s*')
+    local_desc = re.compile(r'\s*description:\s*')
+    local_packager = re.compile(r'\s*packager:\s*(.*)')
+    local_installed = re.compile(r'\s*evr:\s*(\S+)')
+    local_arch = re.compile(r'\s*arch:\s*(\S+)\s*')
 
     def __init__(self, service):
         super().__init__(service, 'zypper', 'Zypper')
@@ -38,9 +42,82 @@ class ZypperSource(AbstractSource):
         start_time = time.perf_counter()
 
         results = []
-        installedids = self._get_installed_ids()
-        apps = self.getapps(installedids.keys())
-        for app in apps:
+        
+        appdatas = {}
+        
+        # Get installed versions for each installed package
+        appid = None
+        arch = None
+        installed_version = None
+        #desc = None
+        #found_desc = False
+        
+        cmd = "zypper search --installed-only --verbose"
+        out = self.executor.call(cmd)
+        for line in out.split("\n"):
+            name_match = self.local_name.match(line)
+            if name_match:
+                if appid is not None:
+                    appdatas["{}|{}".format(appid, arch)] = {
+                        'appid': appid,
+                        'arch': arch,
+                        'installed_version': installed_version,
+                        'available_version': installed_version,
+                        'desc': '',
+                    }
+                desc = None
+                installed_version = None
+                appid = name_match.group(1)
+                continue
+            installed_match = self.local_installed.match(line)
+            if installed_match:
+                installed_version = installed_match.group(1)
+                continue
+            arch_match = self.local_arch.match(line)
+            if arch_match:
+                arch = arch_match.group(1)
+                continue
+        if appid is not None:
+            appdatas["{}|{}".format(appid, arch)] = {
+                'appid': appid,
+                'arch': arch,
+                'installed_version': installed_version,
+                'available_version': installed_version,
+                'desc': '',
+            }
+
+        # Get available versions for each package
+        appid = None
+        arch = None
+        available_version = None
+        
+        cmd = "zypper search --not-installed-only --verbose"
+        out = self.executor.call(cmd)
+        for line in out.split("\n"):
+            name_match = self.local_name.match(line)
+            if name_match:
+                if appid is not None:
+                    appdataid = "{}|{}".format(appid, arch)
+                    if appdataid in appdatas:
+                        appdatas[appdataid]['available_version'] = available_version
+                available_version = None
+                appid = name_match.group(1)
+                continue
+            installed_match = self.local_installed.match(line)
+            if installed_match:
+                available_version = installed_match.group(1)
+                continue
+            arch_match = self.local_arch.match(line)
+            if arch_match:
+                arch = arch_match.group(1)
+                continue
+        if appid is not None:
+            appdataid = "{}|{}".format(appid, arch)
+            if appdataid in appdatas:
+                appdatas[appdataid]['available_version'] = available_version
+        
+        for appdata in appdatas.values():
+            app = App(self, appdata['appid'], appdata['appid'], appdata['desc'], appdata['available_version'], appdata['installed_version'])
             if app is not None and (terms is None or app.match(terms)):
                 results.append(app)
 
@@ -49,63 +126,36 @@ class ZypperSource(AbstractSource):
 
     def getapp(self, appid):
         start_time = time.perf_counter()
-        
-        self.log('performance', "zypper getapp {}".format(time.perf_counter() - start_time))
-        return self.getapps([appid])[0]
 
-    def getapps(self, appids):
-        start_time = time.perf_counter()
-        
-        apps = []
-
-        appid = None
         desc = None
         available_version = None
         installed_version = None
-        
         found_desc = False
-        out = self.executor.call("zypper info {0}".format(' '.join(appids)))
+        
+        out = self.executor.call("zypper info {0}".format(appid))
         for line in out.split("\n"):
-            name_match = self.info_name.match(line)
-            if name_match:
-                if appid in appids:
-                    apps.append(App(self, appid, appid, desc, available_version, installed_version))
-                    
-                appid = name_match.group(1)
-                desc = None
-                available_version = None
-                installed_version = None
-                found_desc = False
-                continue
-
             if found_desc:
                 desc += line.strip()
                 continue
-
             version_match = self.info_version.match(line)
             if version_match:
                 available_version = version_match.group(1)
                 continue
-
             installed_uptodate_match = self.info_installed_uptodate.match(line)
             if installed_uptodate_match:
                 installed_version = available_version
                 continue
-
             installed_outofdate_match = self.info_installed_outofdate.match(line)
             if installed_outofdate_match:
                 installed_version = installed_outofdate_match.group(1)
                 continue
-
             desc_match = self.info_desc.match(line)
             if desc_match:
                 found_desc = True
                 desc = ''
         
-        apps.append(App(self, appid, appid, desc, available_version, installed_version))
-        
-        self.log('performance', "zypper getapps {}".format(time.perf_counter() - start_time))
-        return apps
+        self.log('performance', "zypper getapp {}".format(time.perf_counter() - start_time))
+        return App(self, appid, appid, desc, available_version, installed_version)
 
     def install(self, app):
         self.executor.call("zypper install -y {0}".format(app.id), stdout=self.service.output_std, stderr=self.service.output_err)
