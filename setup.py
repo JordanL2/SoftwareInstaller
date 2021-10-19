6fc20f4aa53312a12207cced01160a6ce59bdb20
Jordan Leppert <jordanleppert@gmail.com>
2021-10-19 13:49:07 +0100

    Version 1.2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="softwareinstaller",
-    version="1.2.3",
+    version="1.2.4",
     author="Jordan Leppert",
     author_email="jordanleppert@gmail.com",
     description="A tool to search, install and update software from multiple sources",
